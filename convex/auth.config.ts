43a0b888b10d4a5d144f68c4da27075039cc7abd
Arafat <arafatmannan2019@gmail.com>
2024-08-15 19:40:48 +0600

    test

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://apparent-shark-59.clerk.accounts.dev/",
+        domain: "https://apparent-shark-59.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
